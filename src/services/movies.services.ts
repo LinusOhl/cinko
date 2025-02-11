bcca12c531923714f0f204d37e8e0d2b48de96a5
Linuss <tade3d@gmail.com>
2025-02-11 12:17:53 +0100

    Fetch movie lists.
    Created types, fetch functions, and query options for handling movie lists fetching.

---
+++ src/services/movies.services.ts
@@ -0,0 +1,61 @@
+import type {
+	MovieListResponse,
+	NowPlayingMoviesResponse,
+} from "../types/movies.types";
+
+export const fetchPopularMovies =
+	async (): Promise<MovieListResponse | null> => {
+		const url = `${import.meta.env.VITE_API_URL}/movies/popular`;
+
+		try {
+			const response = await fetch(url);
+
+			if (!response.ok) {
+				console.error(`HTTP Error: ${response.status} ${response.statusText}`);
+				return null;
+			}
+
+			return (await response.json()) as MovieListResponse;
+		} catch (error: unknown) {
+			console.error(`Fetch failed: ${error}`);
+			return null;
+		}
+	};
+
+export const fetchTopRatedMovies =
+	async (): Promise<MovieListResponse | null> => {
+		const url = `${import.meta.env.VITE_API_URL}/movies/top_rated`;
+
+		try {
+			const response = await fetch(url);
+
+			if (!response.ok) {
+				console.error(`HTTP Error: ${response.status} ${response.statusText}`);
+				return null;
+			}
+
+			return (await response.json()) as MovieListResponse;
+		} catch (error: unknown) {
+			console.error(`Fetch failed: ${error}`);
+			return null;
+		}
+	};
+
+export const fetchNowPlayingMovies =
+	async (): Promise<NowPlayingMoviesResponse | null> => {
+		const url = `${import.meta.env.VITE_API_URL}/movies/now_playing`;
+
+		try {
+			const response = await fetch(url);
+
+			if (!response.ok) {
+				console.error(`HTTP Error: ${response.status} ${response.statusText}`);
+				return null;
+			}
+
+			return (await response.json()) as NowPlayingMoviesResponse;
+		} catch (error: unknown) {
+			console.error(`Fetch failed: ${error}`);
+			return null;
+		}
+	};
