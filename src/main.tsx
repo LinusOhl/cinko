041911c6d62cbf56d981230e015eb1d9e87f5a99
Linuss <tade3d@gmail.com>
2025-02-11 10:57:04 +0100

    Added custom colors.

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,56 @@ const queryClient = new QueryClient();
 
 const theme = createTheme({
 	fontFamily: "Inter, sans-serif",
+	colors: {
+		"r-yellow": [
+			"#fff9e0",
+			"#fff2ca",
+			"#ffe49a",
+			"#fed564",
+			"#fdc838",
+			"#fdc01a",
+			"#fdbb05",
+			"#e1a400",
+			"#c89200",
+			"#ae7d00",
+		],
+		"r-red": [
+			"#ffeaec",
+			"#fdd4d7",
+			"#f3a8ad",
+			"#eb7980",
+			"#e4515a",
+			"#e03742",
+			"#df2935",
+			"#c61b28",
+			"#b11422",
+			"#9c061b",
+		],
+		"r-blue": [
+			"#e6f1ff",
+			"#cddeff",
+			"#9ab9ff",
+			"#6392ff",
+			"#3671ff",
+			"#185cff",
+			"#0152ff",
+			"#0043e5",
+			"#003bce",
+			"#0032b6",
+		],
+		"r-grey": [
+			"#f1f7f1",
+			"#e6e8e6",
+			"#cdcfcd",
+			"#b1b4b1",
+			"#999d99",
+			"#8a8f8a",
+			"#818881",
+			"#6e766e",
+			"#616961",
+			"#505b50",
+		],
+	},
 });
 
 // Set up a Router instance
