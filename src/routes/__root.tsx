e7c837d2879c7a5772e2852ff98df594be7cee10
Linuss <tade3d@gmail.com>
2025-04-11 12:57:43 +0200

    Indentation fixes.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,30 +2,31 @@ import { Anchor, Container, Title } from "@mantine/core";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
-	Link,
-	Outlet,
-	createRootRouteWithContext,
+  Link,
+  Outlet,
+  createRootRouteWithContext,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import "@mantine/core/styles.css";
 import "../index.css";
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
-	{
-		component: RootComponent,
-	},
+  {
+    component: RootComponent,
+    errorComponent: () => <div>Something went wrong!</div>,
+  },
 );
 
 function RootComponent() {
-	return (
-		<Container>
-			<Anchor component={Link} to={"/"} c={"white"} underline="never">
-				<Title order={1}>Revue</Title>
-			</Anchor>
+  return (
+    <Container>
+      <Anchor component={Link} to={"/"} c={"white"} underline="never">
+        <Title order={1}>Revue</Title>
+      </Anchor>
 
-			<Outlet />
-			<ReactQueryDevtools buttonPosition="bottom-left" />
-			<TanStackRouterDevtools position="bottom-right" />
-		</Container>
-	);
+      <Outlet />
+      <ReactQueryDevtools buttonPosition="bottom-left" />
+      <TanStackRouterDevtools position="bottom-right" />
+    </Container>
+  );
 }
