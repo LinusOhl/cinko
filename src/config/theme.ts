fd125ce7b7715511af8bd290380347686ef7cc56
Linuss <tade3d@gmail.com>
2025-06-14 12:38:45 +0200

    Exported CINKO theme to own file.

---
+++ src/config/theme.ts
@@ -0,0 +1,79 @@
+import {
+  type DefaultMantineColor,
+  type MantineColorsTuple,
+  createTheme,
+} from "@mantine/core";
+
+// Typesafety for custom colors
+type ExtendedCustomColors =
+  | "cinkoYellow"
+  | "cinkoRed"
+  | "cinkoBlue"
+  | "cinkoGrey"
+  | DefaultMantineColor;
+
+declare module "@mantine/core" {
+  export interface MantineThemeColorsOverride {
+    colors: Record<ExtendedCustomColors, MantineColorsTuple>;
+  }
+}
+
+// CINKO theme
+export const theme = createTheme({
+  fontFamily: "'Kumbh Sans', sans-serif",
+  headings: {
+    fontFamily: "'Space Grotesk', sans-serif",
+  },
+  black: "#02040F",
+  white: "#E5DADA",
+  colors: {
+    cinkoYellow: [
+      "#fff8e2",
+      "#ffeecd",
+      "#ffdc9c",
+      "#ffc966",
+      "#feb93a",
+      "#feaf1e",
+      "#ffa90f",
+      "#e59500", // main yellow color
+      "#ca8300",
+      "#af7000",
+    ],
+    cinkoRed: [
+      "#ffebed",
+      "#fad2d7",
+      "#f89faa",
+      "#f86b7b",
+      "#f84153",
+      "#f92c3a",
+      "#fa222e",
+      "#df1922",
+      "#c7101d",
+      "#a30015", // main red color
+    ],
+    cinkoBlue: [
+      "#ebeeff",
+      "#d4d8fd",
+      "#a7aef4",
+      "#7781ed",
+      "#4e5ae6",
+      "#3542e2",
+      "#1e2ede", // main blue color
+      "#1929c9",
+      "#1124b4",
+      "#021ea0",
+    ],
+    cinkoGrey: [
+      "#fdf1f1",
+      "#e5dada", // main "white" color
+      "#d3c5c5",
+      "#bba6a6",
+      "#a78a8a",
+      "#9b7979",
+      "#967070",
+      "#835f5e",
+      "#765353",
+      "#6a4646",
+    ],
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,94 +1,39 @@
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
+import { theme } from "./config/theme";
 import { routeTree } from "./routeTree.gen";
 
 const queryClient = new QueryClient();
 
-const theme = createTheme({
-	fontFamily: "Inter, sans-serif",
-	black: "#071013",
-	white: "#E6E8E6",
-	colors: {
-		"r-yellow": [
-			"#fff9e0",
-			"#fff2ca",
-			"#ffe49a",
-			"#fed564",
-			"#fdc838",
-			"#fdc01a",
-			"#fdbb05",
-			"#e1a400",
-			"#c89200",
-			"#ae7d00",
-		],
-		"r-red": [
-			"#ffeaec",
-			"#fdd4d7",
-			"#f3a8ad",
-			"#eb7980",
-			"#e4515a",
-			"#e03742",
-			"#df2935",
-			"#c61b28",
-			"#b11422",
-			"#9c061b",
-		],
-		"r-blue": [
-			"#e6f1ff",
-			"#cddeff",
-			"#9ab9ff",
-			"#6392ff",
-			"#3671ff",
-			"#185cff",
-			"#0152ff",
-			"#0043e5",
-			"#003bce",
-			"#0032b6",
-		],
-		"r-grey": [
-			"#f1f7f1",
-			"#e6e8e6",
-			"#cdcfcd",
-			"#b1b4b1",
-			"#999d99",
-			"#8a8f8a",
-			"#818881",
-			"#6e766e",
-			"#616961",
-			"#505b50",
-		],
-	},
-});
-
 // Set up a Router instance
 const router = createRouter({
-	routeTree,
-	context: {
-		queryClient,
-	},
-	defaultPreload: "intent",
-	defaultPreloadStaleTime: 0,
-	scrollRestoration: true,
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
 });
 
 // Register things for typesafety
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 const rootElement = document.getElementById("app")!;
 if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(
-		<MantineProvider theme={theme}>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-			</QueryClientProvider>
-		</MantineProvider>,
-	);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </MantineProvider>,
+  );
 }
