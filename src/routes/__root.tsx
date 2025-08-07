7228dcc55505d6f35b8f82c5660942fd2cb62ea2
Linuss <tade3d@gmail.com>
2025-08-07 10:55:50 +0200

    Added a simple search bar to the navbar.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,15 @@
-import { Anchor, Container, Title } from "@mantine/core";
+import { Anchor, Container, Flex, TextInput, Title } from "@mantine/core";
+import { useField } from "@mantine/form";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   Link,
   Outlet,
   createRootRouteWithContext,
+  useNavigate,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { FormEvent } from "react";
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import "../global.css";
@@ -19,13 +22,36 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 );
 
 function RootComponent() {
+  const navigate = useNavigate();
+  const field = useField({
+    initialValue: "",
+    validate: (value) =>
+      value.trim().length < 2 ? "Query is too short" : null,
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    field.validate();
+
+    console.log("submitted!", field.getValue());
+
+    navigate({ to: "/search", from: "/", search: { query: field.getValue() } });
+  };
+
   return (
     <Container>
-      <Anchor component={Link} to={"/"} underline="never" preload={false}>
-        <Title order={1} c={"cinkoYellow.7"}>
-          CINKO
-        </Title>
-      </Anchor>
+      <Flex align={"center"} justify={"space-between"}>
+        <Anchor component={Link} to={"/"} underline="never" preload={false}>
+          <Title order={1} c={"cinkoYellow.7"}>
+            CINKO
+          </Title>
+        </Anchor>
+
+        <form onSubmit={handleSubmit}>
+          <TextInput {...field.getInputProps()} placeholder="Search..." />
+        </form>
+      </Flex>
 
       <Outlet />
       <ReactQueryDevtools buttonPosition="bottom-left" />
