c7641d27e515fe8737195291ba23cee821328b9b
Linuss <tade3d@gmail.com>
2025-08-08 08:54:00 +0200

    Added a search field + pagination.

--- src/routes/search/route.tsx
+++ src/routes/search/route.tsx
@@ -1,6 +1,8 @@
-import { Box, Flex, Text } from "@mantine/core";
+import { Box, Center, Flex, Pagination, Text, TextInput } from "@mantine/core";
+import { useField } from "@mantine/form";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { FormEvent, useState } from "react";
 import { z } from "zod";
 import { MovieCard } from "../../components/MovieCard";
 import { searchMoviesQueryOptions } from "../../queryOptions/search.queryOptions";
@@ -8,25 +10,43 @@ import { searchMoviesQueryOptions } from "../../queryOptions/search.queryOptions
 export const Route = createFileRoute("/search")({
   validateSearch: z.object({
     query: z.string(),
+    page: z.number(),
   }),
-  loaderDeps: ({ search: { query } }) => ({ query }),
+  loaderDeps: ({ search: { query, page } }) => ({ query, page }),
   loader: (opts) =>
     opts.context.queryClient.ensureQueryData(
-      searchMoviesQueryOptions(opts.deps.query),
+      searchMoviesQueryOptions(opts.deps.query, Number(opts.deps.page)),
     ),
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const search = Route.useSearch();
+  const navigate = Route.useNavigate();
   const { data: movies } = useSuspenseQuery(
-    searchMoviesQueryOptions(search.query),
+    searchMoviesQueryOptions(search.query, Number(search.page)),
   );
+
+  const field = useField({
+    initialValue: search.query ? search.query : "",
+    validate: (value) =>
+      value.trim().length < 2 ? "Query is too short" : null,
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    navigate({ search: { query: field.getValue(), page: 1 } });
+  };
 
   return (
     <Box mt={"xl"}>
+      <form onSubmit={handleSubmit}>
+        <TextInput {...field.getInputProps()} placeholder="Search..." />
+      </form>
+
       <Text size="xl" fw={600}>
-        Showing results for {search.query}...
+        Showing results for "{search.query}"...
       </Text>
 
       <Flex wrap={"wrap"} gap={"md"} mt={"md"}>
@@ -36,6 +56,16 @@ function RouteComponent() {
             <MovieCard key={movie.id} movie={movie} />
           ))}
       </Flex>
+
+      <Center mt={"md"}>
+        <Pagination
+          value={search.page}
+          onChange={(value) =>
+            navigate({ search: { query: search.query, page: value } })
+          }
+          total={movies.total_pages}
+        />
+      </Center>
     </Box>
   );
 }
