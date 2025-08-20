71b26fd04ffbbf9bc030566012ab6bebca6c1e9b
Linuss <tade3d@gmail.com>
2025-08-20 16:36:00 +0200

    Added dropdown for sort.

---
+++ src/constants/filters.ts
@@ -0,0 +1,10 @@
+export enum SortBy {
+  POPULARITY_ASC = "popularity.asc",
+  POPULARITY_DESC = "popularity.desc",
+  TITLE_ASC = "title.asc",
+  TITLE_DESC = "title.desc",
+  PRIMARY_RELEASE_DATE_ASC = "primary_release_date.asc",
+  PRIMARY_RELEASE_DATE_DESC = "primary_release_date.desc",
+  REVENUE_ASC = "revenue.asc",
+  REVENUE_DESC = "revenue.desc",
+}
--- src/routes/discover/route.tsx
+++ src/routes/discover/route.tsx
@@ -1,7 +1,19 @@
-import { Box, Title } from "@mantine/core";
+import {
+  Box,
+  Center,
+  Combobox,
+  Flex,
+  Input,
+  InputBase,
+  Pagination,
+  Title,
+  useCombobox,
+} from "@mantine/core";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import z from "zod";
+import { MovieCard } from "../../components/MovieCard";
+import { SortBy } from "../../constants/filters";
 import { discoverMovieQueryOptions } from "../../queryOptions/discover.queryOptions";
 
 export const Route = createFileRoute("/discover")({
@@ -19,17 +31,65 @@ export const Route = createFileRoute("/discover")({
 
 function RouteComponent() {
   const search = Route.useSearch();
+  const navigate = Route.useNavigate();
   const { data: movies } = useSuspenseQuery(
     discoverMovieQueryOptions(search.page, search.sortBy),
   );
 
-  console.log("movies:", movies);
+  const combobox = useCombobox();
 
   return (
     <Box mt={"xl"}>
       <Title order={2} c={"cinkoGrey.2"}>
         Discover movies
       </Title>
+
+      <Combobox
+        store={combobox}
+        onOptionSubmit={(val) => {
+          navigate({ search: { page: search.page, sortBy: val } });
+          combobox.closeDropdown();
+        }}
+      >
+        <Combobox.Target>
+          <InputBase
+            component="button"
+            type="button"
+            rightSection={<Combobox.Chevron />}
+            rightSectionPointerEvents="none"
+            onClick={() => combobox.toggleDropdown()}
+            pointer
+          >
+            {search.sortBy || <Input.Placeholder>Pick value</Input.Placeholder>}
+          </InputBase>
+        </Combobox.Target>
+
+        <Combobox.Dropdown>
+          <Combobox.Options>
+            {Object.values(SortBy).map((value) => (
+              <Combobox.Option key={value} value={value}>
+                {value}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        </Combobox.Dropdown>
+      </Combobox>
+
+      <Flex wrap={"wrap"} gap={"md"} mt={"md"}>
+        {movies.results.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </Flex>
+
+      <Center mt={"md"}>
+        <Pagination
+          value={search.page}
+          onChange={(value) =>
+            navigate({ search: { page: value, sortBy: search.sortBy } })
+          }
+          total={movies.total_pages}
+        />
+      </Center>
     </Box>
   );
 }
