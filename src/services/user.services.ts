0740383a3136dc2920d094f05459f9d3c4e3bdf9
Linuss <tade3d@gmail.com>
2025-09-17 14:39:53 +0200

    Fixed rateMovie call.
    * Removed unnecessary try/catch.
    * Throw error on Supabase error.
    * Changed to upsert function.

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -28,40 +28,19 @@ export const rateMovie = async (
   value: number,
   movieId: number,
   userId: string,
-) => {
-  try {
-    const existingRating = await supabase
-      .from("ratings")
-      .select("*")
-      .eq("movie_id", movieId)
-      .eq("user_id", userId)
-      .maybeSingle();
-
-    if (existingRating.data) {
-      await supabase
-        .from("ratings")
-        .update({
-          value: value,
-        })
-        .eq("movie_id", movieId)
-        .eq("user_id", userId);
-
-      return;
-    }
-
-    const { data, error } = await supabase.from("ratings").insert({
-      value,
+): Promise<void> => {
+  const { error } = await supabase.from("ratings").upsert(
+    {
       movie_id: movieId,
       user_id: userId,
-    });
+      value,
+    },
+    {
+      onConflict: "movie_id,user_id",
+    },
+  );
 
-    if (error) {
-      console.error(error.message);
-      throw new Error(error.message);
-    }
-
-    console.log(data);
-  } catch (error: unknown) {
-    throw new Error(`Failed to rate movie: ${error}`);
+  if (error) {
+    throw new Error(`Failed to rate error: ${error.message}`);
   }
 };
