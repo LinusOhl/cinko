5f7a4d8445441f705ee98efad325a16f07641979
Linuss <tade3d@gmail.com>
2026-03-03 11:21:36 +0100

    Redesign: Added a large carousel at the top.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { Carousel, CarouselSlide } from "@mantine/carousel";
-import { Box, Title } from "@mantine/core";
+import {
+  BackgroundImage,
+  Box,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { CustomLink } from "~/components/CustomLink";
 import { MovieCard } from "~/components/MovieCard";
+import { IMAGES_BASE_URL } from "~/helpers";
 import {
   playingMoviesQueryOptions,
   popularMoviesQueryOptions,
@@ -21,15 +29,75 @@ export const Route = createFileRoute("/")({
 });
 
 function IndexView() {
+  const theme = useMantineTheme();
+
   const { data: popularMovies } = useSuspenseQuery(popularMoviesQueryOptions());
-  const { data: topRatedMovies } = useSuspenseQuery(topMoviesQueryOptions());
-  const { data: nowPlayingMovies } = useSuspenseQuery(
-    playingMoviesQueryOptions(),
-  );
+  const { data: topMovies } = useSuspenseQuery(topMoviesQueryOptions());
+  const { data: playingMovies } = useSuspenseQuery(playingMoviesQueryOptions());
 
   return (
-    <div>
-      <Box my={"xl"}>
+    <Stack gap={"xl"}>
+      <Box>
+        <Carousel
+          height={560}
+          slideGap={"md"}
+          slideSize={"100%"}
+          emblaOptions={{ loop: true, dragFree: false, align: "center" }}
+        >
+          {playingMovies.results
+            .filter((movie) => !!movie.backdrop_path)
+            .map((movie) => (
+              <CarouselSlide key={movie.id}>
+                <Box maw={960} h={560}>
+                  <BackgroundImage
+                    src={`${IMAGES_BASE_URL}/original/${movie.backdrop_path}`}
+                    h={560}
+                  >
+                    <Box
+                      style={{
+                        position: "absolute",
+                        inset: 0,
+                        backgroundImage: `linear-gradient(to top, ${theme.black} 5%, transparent 40%)`,
+                      }}
+                    />
+
+                    <Box p={"xs"} style={{ position: "absolute", bottom: 0 }}>
+                      <Text fz={"h2"} ff={"heading"} fw={700}>
+                        {movie.title}
+                      </Text>
+
+                      <Text w={480} lineClamp={3}>
+                        {movie.overview}
+                      </Text>
+                    </Box>
+                  </BackgroundImage>
+                </Box>
+              </CarouselSlide>
+            ))}
+        </Carousel>
+      </Box>
+
+      <Box>
+        <CustomLink to="/" c={"white"} preload={false}>
+          <Title order={2} mb={"xs"}>
+            Top rated movies
+          </Title>
+        </CustomLink>
+
+        <Carousel
+          slideGap={"md"}
+          slideSize={"10%"}
+          emblaOptions={{ dragFree: true }}
+        >
+          {topMovies.results.map((movie) => (
+            <CarouselSlide key={movie.id}>
+              <MovieCard movie={movie} />
+            </CarouselSlide>
+          ))}
+        </Carousel>
+      </Box>
+
+      <Box>
         <CustomLink to="/" c={"white"} preload={false}>
           <Title order={2} mb={"xs"}>
             Popular movies
@@ -48,46 +116,6 @@ function IndexView() {
           ))}
         </Carousel>
       </Box>
-
-      <Box my={"xl"}>
-        <CustomLink to="/" c={"white"} preload={false}>
-          <Title order={2} mb={"xs"}>
-            Top rated movies
-          </Title>
-        </CustomLink>
-
-        <Carousel
-          slideGap={"md"}
-          slideSize={"10%"}
-          emblaOptions={{ dragFree: true }}
-        >
-          {topRatedMovies.results.map((movie) => (
-            <CarouselSlide key={movie.id}>
-              <MovieCard movie={movie} />
-            </CarouselSlide>
-          ))}
-        </Carousel>
-      </Box>
-
-      <Box my={"xl"}>
-        <CustomLink to="/" c={"white"} preload={false}>
-          <Title order={2} mb={"xs"}>
-            Now playing movies
-          </Title>
-        </CustomLink>
-
-        <Carousel
-          slideGap={"md"}
-          slideSize={"10%"}
-          emblaOptions={{ dragFree: true }}
-        >
-          {nowPlayingMovies.results.map((movie) => (
-            <CarouselSlide key={movie.id}>
-              <MovieCard movie={movie} />
-            </CarouselSlide>
-          ))}
-        </Carousel>
-      </Box>
-    </div>
+    </Stack>
   );
 }
