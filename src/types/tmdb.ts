9a9d9f7bd7b806402680ffbecaecbb0abf8c4a78
Linuss <tade3d@gmail.com>
2026-03-03 16:01:06 +0100

    Added a detailed movie type.

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -20,6 +20,36 @@ export type TMDBMovie = {
   vote_count: number;
 };
 
+export type TMDBMovieDetails = Omit<TMDBMovie, "genre_ids"> & {
+  belongs_to_collection: string;
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  imdb_id: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+};
+
 export type TMDBPlayingMovies = TMDBBaseList<TMDBMovie> & {
   dates: {
     maximum: string;
