8cb95791bfce470ab3d91c4115de30fee5834eb0
Linuss <tade3d@gmail.com>
2026-03-10 10:57:05 +0100

    Added name, biography, and profile image.

--- src/routes/people/$personId.tsx
+++ src/routes/people/$personId.tsx
@@ -1,5 +1,7 @@
+import { Group, Image, Stack, Text, Title } from "@mantine/core";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { IMAGES_BASE_URL } from "~/helpers";
 import { personQueryOptions } from "~/queries/people";
 
 export const Route = createFileRoute("/people/$personId")({
@@ -13,8 +15,19 @@ function RouteComponent() {
   const { data: person } = useSuspenseQuery(personQueryOptions(personId));
 
   return (
-    <div>
-      <p>person details page for {person.id}</p>
-    </div>
+    <Stack my={"xl"}>
+      <Group align="flex-start" wrap="nowrap">
+        <Image
+          src={`${IMAGES_BASE_URL}/h632/${person.profile_path}`}
+          w={312}
+          radius={"md"}
+        />
+
+        <Stack>
+          <Title order={1}>{person.name}</Title>
+          <Text>{person.biography}</Text>
+        </Stack>
+      </Group>
+    </Stack>
   );
 }
