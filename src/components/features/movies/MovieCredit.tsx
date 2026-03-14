9cacfdf5188a62af15c349c810fe627af62c9f6f
Linuss <tade3d@gmail.com>
2026-03-14 15:27:37 +0100

    Created movie credit component.

---
+++ src/components/features/movies/MovieCredit.tsx
@@ -0,0 +1,33 @@
+import { Image, Tooltip } from "@mantine/core";
+import { useNavigate } from "@tanstack/react-router";
+import { IMAGES_BASE_URL } from "~/helpers";
+import type { TMDBMovie } from "~/types/tmdb";
+
+interface MovieCreditProps {
+  movie: TMDBMovie;
+}
+
+export const MovieCredit = ({ movie }: MovieCreditProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Tooltip label={movie.title} position="bottom" withArrow>
+      <Image
+        src={
+          movie.poster_path
+            ? `${IMAGES_BASE_URL}/w154/${movie.poster_path}`
+            : null
+        }
+        alt={movie.title}
+        w={96}
+        style={{ cursor: "pointer" }}
+        onClick={() =>
+          navigate({
+            to: "/movies/$movieId/details",
+            params: { movieId: movie.id.toString() },
+          })
+        }
+      />
+    </Tooltip>
+  );
+};
