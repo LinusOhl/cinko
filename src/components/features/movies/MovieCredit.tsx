16c9553bcacd4ef997876b5883e2fd2badd1e507
Linuss <tade3d@gmail.com>
2026-03-17 10:59:33 +0100

    Show box with movie title if no poster.

--- src/components/features/movies/MovieCredit.tsx
+++ src/components/features/movies/MovieCredit.tsx
@@ -1,4 +1,4 @@
-import { Image, Tooltip } from "@mantine/core";
+import { Box, Image, Text, Tooltip } from "@mantine/core";
 import { useNavigate } from "@tanstack/react-router";
 import { IMAGES_BASE_URL } from "~/helpers";
 import type { TMDBMovie } from "~/types/tmdb";
@@ -12,22 +12,38 @@ export const MovieCredit = ({ movie }: MovieCreditProps) => {
 
   return (
     <Tooltip label={movie.title} position="bottom" withArrow>
-      <Image
-        src={
-          movie.poster_path
-            ? `${IMAGES_BASE_URL}/w154/${movie.poster_path}`
-            : null
-        }
-        alt={movie.title}
-        w={96}
-        style={{ cursor: "pointer" }}
-        onClick={() =>
-          navigate({
-            to: "/movies/$movieId/details",
-            params: { movieId: movie.id.toString() },
-          })
-        }
-      />
+      {movie.poster_path ? (
+        <Image
+          src={
+            movie.poster_path
+              ? `${IMAGES_BASE_URL}/w154/${movie.poster_path}`
+              : null
+          }
+          alt={movie.title}
+          w={96}
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            navigate({
+              to: "/movies/$movieId/details",
+              params: { movieId: movie.id.toString() },
+            })
+          }
+        />
+      ) : (
+        <Box
+          bg={"dark"}
+          w={96}
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            navigate({
+              to: "/movies/$movieId/details",
+              params: { movieId: movie.id.toString() },
+            })
+          }
+        >
+          <Text ta={"center"}>{movie.title}</Text>
+        </Box>
+      )}
     </Tooltip>
   );
 };
