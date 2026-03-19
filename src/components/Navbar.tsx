a5c82a14e07bc38650cb4d4591ac24a3d6877e7d
Linuss <tade3d@gmail.com>
2026-03-19 13:26:00 +0100

    Show logout button if logged in.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import { Button, Divider, Group } from "@mantine/core";
 import { useNavigate } from "@tanstack/react-router";
+import { authClient } from "~/lib/auth-client";
 import { CustomLink } from "./CustomLink";
 
 export const Navbar = () => {
   const navigate = useNavigate();
+
+  const { data: session } = authClient.useSession();
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  };
 
   return (
     <>
@@ -13,16 +20,27 @@ export const Navbar = () => {
         </CustomLink>
 
         <Group>
-          <Button
-            variant="light"
-            color="cinkoBlue"
-            onClick={() => navigate({ to: "/signin" })}
-          >
-            Sign in
-          </Button>
-          <Button color="cinkoBlue" onClick={() => navigate({ to: "/signup" })}>
-            Sign up
-          </Button>
+          {session ? (
+            <Button variant="light" color="cinkoRed" onClick={handleSignOut}>
+              Log out
+            </Button>
+          ) : (
+            <>
+              <Button
+                variant="light"
+                color="cinkoBlue"
+                onClick={() => navigate({ to: "/signin" })}
+              >
+                Sign in
+              </Button>
+              <Button
+                color="cinkoBlue"
+                onClick={() => navigate({ to: "/signup" })}
+              >
+                Sign up
+              </Button>
+            </>
+          )}
         </Group>
       </Group>
 
