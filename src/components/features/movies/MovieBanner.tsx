d126a876dbb8c556b6fa36f2cb245c81e757dbb0
Linuss <tade3d@gmail.com>
2026-03-20 14:51:40 +0100

    Formatting.

--- src/components/features/movies/MovieBanner.tsx
+++ src/components/features/movies/MovieBanner.tsx
@@ -20,11 +20,11 @@ export const MovieBanner = ({ movie }: MovieBannerProps) => {
         {/* Fade-effect of backdrop image */}
         <Overlay
           bg={`
-              radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 60%, #02040f 100%), 
-              linear-gradient(to bottom, rgba(0, 0, 0, 0) 70%, #02040f 100%),
-              linear-gradient(to left, rgba(0, 0, 0, 0) 90%, #02040f 100%),
-              linear-gradient(to right, rgba(0, 0, 0, 0) 90%, #02040f 100%)
-            `}
+            radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 60%, #02040f 100%), 
+            linear-gradient(to bottom, rgba(0, 0, 0, 0) 70%, #02040f 100%),
+            linear-gradient(to left, rgba(0, 0, 0, 0) 90%, #02040f 100%),
+            linear-gradient(to right, rgba(0, 0, 0, 0) 90%, #02040f 100%)
+          `}
         />
       </Box>
     </Skeleton>
