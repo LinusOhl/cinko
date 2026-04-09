0b81801d38f53bd69a71e7fd99e1a92a55567ced
Linuss <tade3d@gmail.com>
2026-04-09 13:55:48 +0200

    Image optimization + styling changes.

--- src/components/features/movies/MoviePoster/index.tsx
+++ src/components/features/movies/MoviePoster/index.tsx
@@ -10,11 +10,21 @@ interface MoviePosterProps {
 /* TODO: set up fallback src/element */
 
 export const MoviePoster = ({ posterPath, width }: MoviePosterProps) => {
+  const getPosterSize = (width: number): string => {
+    const targetWidth = width * 1.5;
+
+    const sizes = [92, 154, 185, 342, 500, 780];
+
+    const bestSize = sizes.find((size) => size >= targetWidth);
+
+    return bestSize ? `w${bestSize}` : "original";
+  };
+
   return (
     <Box w={width}>
       <div
         style={{
-          backgroundImage: `url(${IMAGES_BASE_URL}/w500/${posterPath})`,
+          backgroundImage: `url(${IMAGES_BASE_URL}/${getPosterSize(width)}/${posterPath})`,
           width: width,
         }}
         className={classes.movieCardImageBox}
