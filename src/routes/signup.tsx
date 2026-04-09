24adb2a77b1c92a9d7a948005867c5e4ee9fde20
Linuss <tade3d@gmail.com>
2026-04-09 10:28:10 +0200

    Users can signup using email and password.

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Button,
   Card,
   Group,
@@ -8,14 +9,67 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
-import { createFileRoute } from "@tanstack/react-router";
+import { schemaResolver, useForm } from "@mantine/form";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+import z from "zod";
 import { CustomLink } from "~/components/CustomLink";
+import { authClient } from "~/lib/auth-client";
+
+const SignUpSchema = z
+  .object({
+    name: z.string().min(2),
+    email: z.email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8),
+  })
+  .refine(
+    (values) => {
+      return values.confirmPassword === values.password;
+    },
+    {
+      error: "Passwords does not match.",
+      path: ["confirmPassword"],
+    },
+  );
 
 export const Route = createFileRoute("/signup")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validate: schemaResolver(SignUpSchema, { sync: true }),
+  });
+
+  const handleSignUp = async (
+    name: string,
+    email: string,
+    password: string,
+  ) => {
+    await authClient.signUp.email(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        onError: ({ error }) => setError(error.message),
+        onSuccess: () => navigate({ to: "/" }),
+      },
+    );
+  };
+
   return (
     <Stack h={"100vh"} p={"xs"} gap={"xl"} justify="center">
       <Title order={1} fz={96} styles={{ root: { textAlign: "center" } }}>
@@ -35,34 +89,40 @@ function RouteComponent() {
 
         <Card p={"lg"} radius={"md"} shadow="md" withBorder>
           <Stack gap={"md"}>
+            {error && (
+              <Alert title="Error" color="cinkoRed" radius={"sm"}>
+                {error}
+              </Alert>
+            )}
+
             <form
-            // onSubmit={form.onSubmit((values) =>
-            //   handleSignUp(values.name, values.email, values.password),
-            // )}
+              onSubmit={form.onSubmit((values) =>
+                handleSignUp(values.name, values.email, values.password),
+              )}
             >
               <Stack gap={"sm"}>
                 <TextInput
-                  // key={form.key("name")}
+                  key={form.key("name")}
                   label="Name"
-                  // {...form.getInputProps("name")}
+                  {...form.getInputProps("name")}
                 />
 
                 <TextInput
-                  // key={form.key("email")}
+                  key={form.key("email")}
                   label="Email"
-                  // {...form.getInputProps("email")}
+                  {...form.getInputProps("email")}
                 />
 
                 <PasswordInput
-                  // key={form.key("password")}
+                  key={form.key("password")}
                   label="Password"
-                  // {...form.getInputProps("password")}
+                  {...form.getInputProps("password")}
                 />
 
                 <PasswordInput
-                  // key={form.key("confirmPassword")}
+                  key={form.key("confirmPassword")}
                   label="Confirm password"
-                  // {...form.getInputProps("confirmPassword")}
+                  {...form.getInputProps("confirmPassword")}
                 />
 
                 <Button type="submit" color="cinkoBlue" fullWidth>
