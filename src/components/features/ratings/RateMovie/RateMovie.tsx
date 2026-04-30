10aa046b7c37f7b74fad0e1f97ae10dc7ca993c3
Linuss <tade3d@gmail.com>
2026-04-30 12:36:27 +0200

    Handle rating of movie.

--- src/components/features/ratings/RateMovie/RateMovie.tsx
+++ src/components/features/ratings/RateMovie/RateMovie.tsx
@@ -10,9 +10,17 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
+import { useRateMovieMutation } from "~/server/db/ratings/ratings.queries";
+import type { TMDBMovieDetails } from "~/types/tmdb";
 import { ratingCategories, scoreMarks } from "./helpers";
 
-export const RateMovie = () => {
+interface RateMovieProps {
+  movie: TMDBMovieDetails;
+}
+
+export const RateMovie = ({ movie }: RateMovieProps) => {
+  const { mutate, isPending } = useRateMovieMutation();
+
   const [opened, { open, close }] = useDisclosure(false);
   const [overallValue, setOverallValue] = useState<number>(0);
 
@@ -22,7 +30,9 @@ export const RateMovie = () => {
 
   const handleOverallRatingChange = (value: number) => {
     // Reset all individual ratings
-    setRatings(Object.fromEntries(ratingCategories.map((cat) => [cat.key, 1])));
+    setRatings(
+      Object.fromEntries(ratingCategories.map((cat) => [cat.key, value])),
+    );
 
     setOverallValue(value);
   };
@@ -35,6 +45,17 @@ export const RateMovie = () => {
     // Reset all ratings
     setRatings(Object.fromEntries(ratingCategories.map((cat) => [cat.key, 1])));
     setOverallValue(0);
+
+    close();
+  };
+
+  const handleSubmit = () => {
+    mutate({
+      data: {
+        movie,
+        ratings,
+      },
+    });
 
     close();
   };
@@ -80,18 +101,21 @@ export const RateMovie = () => {
                 min={1}
                 max={5}
                 label={(val) =>
-                  scoreMarks.find((mark) => mark.value === val)!.label
+                  scoreMarks.find((mark) => mark.value === val)?.label
                 }
                 marks={scoreMarks}
                 color={category.color}
                 styles={{ markLabel: { display: "none" } }}
+                disabled={overallValue > 0}
               />
             </Stack>
           ))}
+
+          <Button onClick={handleSubmit}>Submit rating</Button>
         </Stack>
       </Modal>
 
-      <Button color="cinkoYellow" onClick={open}>
+      <Button color="cinkoYellow" onClick={open} loading={isPending}>
         Rate movie
       </Button>
     </>
